d522a49e7b1590afb617c1ae4ba2524ecd660cb3
heyitsgrace996 <139715656+heyitsgrace996@users.noreply.github.com>
2023-11-30 11:02:44 +0000

    Added menu skeleton. Missing functions are commented with "TODO"

--- src/main/java/libmansys/Main.java
+++ src/main/java/libmansys/Main.java
@@ -17,7 +17,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+ 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
