7fa72f4d63c4f2565bf9125cbd6c877941b0ac66
LeoOrtin <leoortin15@gmail.com>
2023-12-02 15:44:36 +0000

    Updating MergeSortTest

--- src/test/java/libmansys/MergeSortTest.java
+++ src/test/java/libmansys/MergeSortTest.java
@@ -7,33 +7,50 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortTest
 {
-
     private ArrayList<LibItem> itemsList;
     private Book book;
+    private Book book1;
     private Media media;
     private Thesis thesis;
 
     @Test
-    void testMergeSort() {
+    void testMergeSort() throws LibItemException, ParseException {
+        addingItemsToList();
+        MergeSort<LibItem> sorter = new MergeSort<>();
+        Comparator<LibItem> comparator = Comparator.comparing(LibItem::getTitle);
+
+        sorter.sort(itemsList, comparator);
+
+        ArrayList<LibItem> expectedList = new ArrayList<>();
+        expectedList.add(book1);
+        expectedList.add(book);
+        expectedList.add(media);
+        expectedList.add(thesis);
+
+        assertEquals(expectedList, itemsList);
     }
 
-    private void addingItems() throws ParseException, LibItemException {
+    private void addingItemsToList() throws ParseException, LibItemException {
         itemsList = new ArrayList<>();
         book = new Book("Book Title", true, "Author", "0000000000000",
+                "00000000-0000-0000-0000-000000000000");
+        book1 = new Book("Book Title", true, "Author", "0000000000000",
                 "00000000-0000-0000-0000-000000000000");
         media = new Media("Media Title", true, "Producer", "Director",
                 Duration.ofHours(1).plusMinutes(30), "00000000-0000-0000-0000-000000000000");
         thesis = new Thesis("Thesis Title", true, "Author", "Topic",
                 "Abstract Summary", new SimpleDateFormat("dd/MM/yyyy").parse("14/05/2023"),
                 "00000000-0000-0000-0000-000000000000");
-        itemsList.add(book);
         itemsList.add(media);
+        itemsList.add(book1);
         itemsList.add(thesis);
+        itemsList.add(book);
     }
 
 }
